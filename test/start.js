ee360c92af26a9c9cdd12aecf4005e6bb46a0f7a
t-hrhs <hirohashi.t.0204@gmail.com>
2015-02-14 21:10:29 +0900

    can load html file by node.js

--- test/start.js
+++ test/start.js
@@ -1,6 +1,14 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var fs = require('fs');
+var server = http.createServer();
+server.on('request', doRequest);
+server.listen(1337);
 console.log('Server runnning at local host port 1337');
+
+function doRequest (req, res) {
+    fs.readFile('./hello.html', 'UTF-8', function(err,data) {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write(data);
+        res.end();
+    });
+}
